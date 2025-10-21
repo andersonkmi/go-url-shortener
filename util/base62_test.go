3fe57d163bbafd640ba39eaf8e861b0653c3b759
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2025-10-21 21:07:02 +0200

    Add test cases

--- util/base62_test.go
+++ util/base62_test.go
@@ -10,3 +10,21 @@ func TestConvertZeroToBase62(t *testing.T) {
 		t.Errorf("Expected 0 to be converted to 0, got %s", result)
 	}
 }
+
+func TestConvertNumberToBase62(t *testing.T) {
+	testcases := []struct {
+		in   int64
+		want string
+	}{
+		{0, "0"},
+		{123, "1z"},
+		{29290292, "1ytk4"},
+	}
+
+	for _, tc := range testcases {
+		result := idToBase62(tc.in)
+		if result != tc.want {
+			t.Errorf("Expected %d to be converted to %s, got %s", tc.in, tc.want, result)
+		}
+	}
+}
