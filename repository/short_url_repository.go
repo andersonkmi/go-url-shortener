4d937bd8e3e5ccb0827d1148fc7e7f368f7782ea
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2025-12-01 22:52:34 +0100

    Minor formatting fix

--- repository/short_url_repository.go
+++ repository/short_url_repository.go
@@ -15,7 +15,7 @@ func GenerateShortUrlId(connection *sql.DB) (int64, error) {
 	var urlId int64
 	err := connection.QueryRow("select nextval('url_id_sequence')").Scan(&urlId)
 	if err != nil {
-		return -1, fmt.Errorf("Failed to generate short url id: %v", err)
+		return -1, fmt.Errorf("failed to generate short url id: %v", err)
 	}
 	return urlId, nil
 }
