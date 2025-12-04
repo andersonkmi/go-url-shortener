3e0fdd6cfdd629a4c18fdb01c0462ae8c0158509
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2025-12-04 22:47:47 +0100

    Refactor response generation

--- api_handler.go
+++ api_handler.go
@@ -23,53 +23,51 @@ func redirectHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, longUrl, http.StatusFound)
 }
 
-func shortenHandler(w http.ResponseWriter, r *http.Request) {
+func shortenHandler(writer http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		http.Error(writer, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
 	var createUrlRequest CreateURLRequest
 	if err := json.NewDecoder(r.Body).Decode(&createUrlRequest); err != nil {
-		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		http.Error(writer, "Invalid request body", http.StatusBadRequest)
 		return
 	}
 
 	err := validateUrl(createUrlRequest.URL)
 	if err != nil {
 		log.Println(err)
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(writer, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	shortenedUrl, err := shortener.ShortenUrl(createUrlRequest.URL)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	shortUrl := fmt.Sprintf("http://localhost:8080/%s", shortenedUrl)
-	fmt.Sprintln(w, "Short URL created: %s", shortUrl)
+	fmt.Sprintln(writer, "Short URL created: %s", shortUrl)
 
-	var urlResponse = URLResponse{createUrlRequest.URL, shortUrl}
-	respondWithJSON(w, http.StatusOK, urlResponse)
+	generateSuccessResponse(writer, http.StatusCreated, createUrlRequest.URL, shortUrl)
 }
 
-func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	response, _ := json.Marshal(payload)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	w.Write(response)
+func generateSuccessResponse(writer http.ResponseWriter, code int, originalUrl string, shortenedUrl string) {
+	var urlResponse = URLResponse{originalUrl, shortenedUrl}
+	response, _ := json.Marshal(urlResponse)
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(code)
+	writer.Write(response)
 }
 
 func validateUrl(url string) error {
 	if url == "" {
 		return fmt.Errorf("URL is empty")
 	}
-
 	if !(strings.HasPrefix(url, "http://") || strings.HasPrefix(url, "https://")) {
 		return fmt.Errorf("URL must start with http or https")
 	}
-
 	return nil
 }
