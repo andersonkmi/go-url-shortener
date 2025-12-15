bfda0798e5a57806cb7b14c22678f347f5148f2f
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2025-12-15 23:44:58 +0100

    Add db connection closing handling

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"go-url-shortener/config"
 	"go-url-shortener/handlers"
 	"go-url-shortener/internal"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -15,13 +21,41 @@ func main() {
 	if err := internal.InitDB(appConfig); err != nil {
 		log.Fatal(err)
 	}
-	defer internal.CloseDB()
 
 	log.Info("Starting application...")
 	http.HandleFunc("/", handlers.RedirectHandler)
 	http.HandleFunc("/shorten", handlers.ShortenHandler)
 	http.HandleFunc("/health", handlers.HealthCheckHandler)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", appConfig.ApplicationPort), nil))
+	httpServer := &http.Server{Addr: fmt.Sprintf(":%d", appConfig.ApplicationPort)}
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		if err := httpServer.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.WithError(err).Fatal("Server error")
+		}
+	}()
+
+	log.Info("Server started")
+
+	// Block until signal received
+	<-sigChan
+	log.Info("Shutting down...")
+
+	// Create shutdown context with timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Shutdown HTTP server gracefully
+	if err := httpServer.Shutdown(ctx); err != nil {
+		log.WithError(err).Warn("Server shutdown error")
+	}
+
+	// Close database connection
+	if err := internal.CloseDB(); err != nil {
+		log.WithError(err).Warn("Database close error")
+	}
 }
 
 func init() {
